4e068b479d9c3633fce9da4e763893e3f9d4a306
”kensuke” <kensuke1202left@icloud.com>
2020-11-11 19:34:22 +0900

    access.php 修正

--- php_part9/access.php
+++ php_part9/access.php
@@ -1,9 +1,18 @@
 <?php
-foreach(file("people.txt") as $line){
+// foreach(file("people.txt") as $line){
+//     $line = trim($line);
+//     $info = explode('|', $line);
+//     //mail作成リンク生成
+//     print '<li><a href="mailto:' . $info[0] .'">' . $info[1]. '</a></li><br>';
+// }
+
+$fh = fopen('people.txt', 'rb');
+// 取得したファイルの現在位置が末尾に達していない&&ファイルの行の情報が取得できている
+while((!feof($fh)) && ($line = fgets($fh))){
     $line = trim($line);
     $info = explode('|', $line);
-    //mail作成リンク生成
     print '<li><a href="mailto:' . $info[0] .'">' . $info[1]. '</a></li><br>';
 }
+fclose($fh); //ファイルへの接続解除
 
 ?>
