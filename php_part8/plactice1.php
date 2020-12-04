fd82e2ab8da5b4b87613e657ea0e76eb51580a5e
”kensuke” <kensuke1202left@icloud.com>
2020-12-05 00:00:43 +0900

    演習問題１追加

--- php_part8/plactice1.php
+++ php_part8/plactice1.php
@@ -1,24 +1,39 @@
 <?php
 //演習問題8.12-1
-"UPDATE dishes ORDER BY price ";
+"create table dishes(
+    dishes_id INT,
+    dishe_name VARCHAR(255),
+    price DECIMAL(4, 2),
+    is_spicy INT
+)";
+
+// INSERT INTO dishes VALUES (1, "Walnut Bun", 1.00, 0);
+// INSERT INTO dishes VALUES (2, "Cashew Nuts and White Mushrooms", 4.95, 0);
+// INSERT INTO dishes VALUES (3, "DriedMulberries", 3.00, 0);
+// INSERT INTO dishes VALUES (4, "Eggplant with Chili Sauce", 6.50, 1);
+// INSERT INTO dishes VALUES (5, "Red Bean Bun", 1.00, 0);
+// INSERT INTO dishes VALUES (6, "General Tso's Chickin", 5.50, 1);
 
 try{
-    $db = new PDO('mysql:host=web_mysql_1;dbname=plactice','root','pass');
+    $db = new PDO('mysql:host=docker-practice_mysql_1;dbname=plactice','root','pass');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
     $sql = "SELECT price FROM dishes ORDER BY price";
+    // $sql = "SELECT * FROM dishes WHERE dish_id=?";
     $stmt = $db->query($sql);
     $dishes = $stmt->fetchAll();
+
+    var_dump($stmt);
 
     if(count($dishes) == 0){
         print 'No dishes matched';
     }else{
         print '<table>';
-        print '"<tr><th>dish Name</th><th>Price</th><th>Spicy?</th></tr>';
+        print '<tr><th>Price</th></tr>';
         foreach($dishes as $dish){
-            print "<tr><td>".
-            printf('<tr><td>%s</td><td>$%.02f</td><td>%s</td></tr>',
-                htmlentities($dish->dish_name), $dish->price, $dish->is_spicy);
+            printf('<tr><td>$%.02f</td></tr>',
+                htmlentities($dish->price));
         }
         print '</table>';
     }
